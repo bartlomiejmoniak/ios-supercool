5eff3b73da4bb8003bcd534304d42dd0886c2128
Bartłomiej <bm@Bartlomiejs-MacBook-Pro.local>
2016-04-20 21:41:36 +0200

    added files for supercool

---
+++ Scndgittest/Scndgittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scndgittest
+//
+//  Created by Bartłomiej on 12/04/16.
+//  Copyright © 2016 Bartłomiej Moniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
